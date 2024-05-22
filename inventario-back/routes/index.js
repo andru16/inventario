b55f8ff95316c2232fdfb797f89d865c647ff8af
Manuel Castro <manuelcastro911@gmail.com>
2024-05-22 18:55:56 -0500

    actualizacion rutasa

--- inventario-back/routes/index.js
+++ inventario-back/routes/index.js
@@ -4,12 +4,11 @@ const articulosRouter = require('./articulos.router')
 const inventarioRouter = require('./inventario.router')
 const categoriaRouter = require('./categorias.router')
 const registroRouter = require('./registro.router')
-const proveedorRouter = require('./proveedores.router')
+// const proveedorRouter = require('./proveedores.router')
 
 function routerApi(app) {
   app.use('/auth',authRouter)
   app.use('/articulos', articulosRouter)
-  app.use('/proveedores', proveedorRouter)
   app.use('/inventarios', inventarioRouter)
   app.use('/categorias', categoriaRouter)
   app.use('/registro', registroRouter)
